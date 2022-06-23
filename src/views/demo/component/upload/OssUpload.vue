4fdc7499c4d4959a5967e4e34c42723b68ec846f
shiwuhao <shiwuhao@juzishuke.com>
2022-06-23 18:37:39 +0800

    form-upload

--- src/views/demo/component/upload/OssUpload.vue
+++ src/views/demo/component/upload/OssUpload.vue
@@ -7,7 +7,7 @@ import {BasicUpload} from '~/components/Upload';
 import {reactive, ref} from "vue";
 import {UploadProps, UploadRawFile, UploadRequestOptions} from "element-plus";
 import {fetchOssSign} from "~/api/upload";
-import axios from "~/utils/axios";
+import axios from "axios";
 
 const uploadProps = reactive<Partial<UploadProps>>({
   action: import.meta.env.VITE_UPLOAD_URL,
@@ -16,19 +16,20 @@ const uploadProps = reactive<Partial<UploadProps>>({
 });
 const files = ref([])
 const httpRequest = async (options: UploadRequestOptions) => {
-  const {data:{data:{accessid, host, policy, dir, signature}}} = await fetchOssSign();
+  const {data:{data:{accessid, host, policy, dir, signature,callback}}} = await fetchOssSign();
 
   const formData = new FormData();
-  formData.append('name', options.filename)
-  formData.append('key', `${dir}/${options.filename}`)
+  formData.append('name', options.file.name)
+  formData.append('key', `${dir}/${options.file.name}`)
   formData.append('policy', policy)
   formData.append('OSSAccessKeyId', accessid)
   formData.append('success_action_status', '200')
   formData.append('signature', signature)
+  formData.append('callback', callback)
   formData.append('file', options.file)
-  console.log(host)
-  axios.post(host,formData).then(res=>{
-    console.log(res);
+  axios.post(host,formData).then(response=>{
+    const file = response.data?.data;
+    console.log(file);
   })
 }
 </script>
