f90b4f718049ece9da63159bf9370083fb8a0d4d
shiwuhao <shiwuhao@juzishuke.com>
2022-06-24 14:32:35 +0800

    form-upload

--- src/views/demo/component/upload/OssUpload.vue
+++ src/views/demo/component/upload/OssUpload.vue
@@ -1,5 +1,7 @@
 <template>
-  <BasicUpload v-model="files" v-bind="uploadProps" :http-request="httpRequest"></BasicUpload>
+  <PageWrapper :title="$route['meta']['title']" content-full-height content-background>
+    <BasicUpload v-model="files" v-bind="uploadProps" :before-upload="handleBeforeUpload"></BasicUpload>
+  </PageWrapper>
 </template>
 
 <script lang="ts" setup>
@@ -9,14 +11,21 @@ import {UploadProps, UploadRawFile, UploadRequestOptions} from "element-plus";
 import {fetchOssSign} from "~/api/upload";
 import axios from "axios";
 
-const uploadProps = reactive<Partial<UploadProps>>({
-  action: import.meta.env.VITE_UPLOAD_URL,
+const uploadProps = reactive({
+  action: '',
   multiple: true,
-  data: {}
+  data: {
+    key: '',
+    policy: '',
+    OSSAccessKeyId: '',
+    success_action_status: '200',
+    signature: '',
+    callback: ''
+  }
 });
 const files = ref([])
 const httpRequest = async (options: UploadRequestOptions) => {
-  const {data:{data:{accessid, host, policy, dir, signature,callback}}} = await fetchOssSign();
+  const {data: {data: {accessid, host, policy, dir, signature, callback}}} = await fetchOssSign();
 
   const formData = new FormData();
   formData.append('name', options.file.name)
@@ -27,10 +36,24 @@ const httpRequest = async (options: UploadRequestOptions) => {
   formData.append('signature', signature)
   formData.append('callback', callback)
   formData.append('file', options.file)
-  axios.post(host,formData).then(response=>{
+  axios.post(host, formData).then(response => {
     const file = response.data?.data;
     console.log(file);
   })
+}
+
+const handleBeforeUpload = async (rawFile: UploadRawFile) => {
+  console.log(rawFile);
+  const {data: {data: {accessid, host, policy, dir, signature, callback}}} = await fetchOssSign();
+  uploadProps.action = host;
+  uploadProps.data = {
+    key: `${dir}/${rawFile.name}`,
+    policy: policy,
+    OSSAccessKeyId: accessid,
+    success_action_status: '200',
+    signature: signature,
+    callback: callback,
+  }
 }
 </script>
 
