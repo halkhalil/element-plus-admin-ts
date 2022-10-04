30c9ea6eeacd6407b2a7948a373c79b9acdda650
shiwuhao <shiwuhao@juzifenqi.com>
2022-10-04 22:36:28 +0800

    layout

--- src/layouts/iframe/index.vue
+++ src/layouts/iframe/index.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="h-full" v-loading="loading">
-    <iframe class="h-full w-full border-0" :src="currentRoute?.meta?.frameSrc" @load="hideLoading"></iframe>
+    <iframe ref="contentElRef" class="w-full border-0" :style="getContentHeight" :src="currentRoute?.meta?.frameSrc"
+            @load="hideLoading"></iframe>
   </div>
 </template>
 
 <script setup lang="ts">
 import {useRouter} from "vue-router";
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import {useElementBounding} from "@vueuse/core";
 
 const {currentRoute} = useRouter();
 const loading = ref(true);
 const hideLoading = () => loading.value = false;
+const contentElRef = ref<HTMLElement>()
+const {top} = useElementBounding(contentElRef);
+const getContentHeight = computed(() => {
+  return {'minHeight':document.documentElement.clientHeight - top.value -10 + 'px'};
+})
 </script>
-<style lang="scss" >
-.el-scrollbar__view{
+<style lang="scss">
+.el-scrollbar__view {
   height: 100%;
 }
 </style>
